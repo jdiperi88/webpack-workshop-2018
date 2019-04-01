24737b22199d291461b5a26098f5eaa143ebf103
jdiperi88 <jdiperi88@yahoo.com>
2019-03-31 23:01:46 -0400

    new branch

---
+++ src/button.js
@@ -0,0 +1,4 @@
+//take a str, the button label and return a element
+const makeButton = buttonName => `Button: ${buttonName}`;
+
+module.exports = makeButton;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import nav from "./nav";
+import { top, bottom } from "./footer";
+import buttonName from "./button";
+import { makeColorStyle } from "./button-styles";
+console.log(nav, bottom, top, buttonName("my button"), makeColorStyle("cyan"));
